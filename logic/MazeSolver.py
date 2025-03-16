85743fc2e2535afd5a8b33b254ee4b0ebc0d9ba5
anderson_pc <anderson19pc98@gmail.com>
2025-03-16 10:23:18 -0500

    se guarda la solucion y se aplica movimiento del personaje en el grafico

--- logic/MazeSolver.py
+++ logic/MazeSolver.py
@@ -1,11 +1,14 @@
 from collections import deque
 from logic.Node import Node
 import numpy as np
+import time
+import pygame
 
 class MazeSolver:
     """ Representa el laberinto original y lo resuelve con base a distintas funciones de búsqueda  """
 
-    def __init__(self, maze_loader=None):
+    def __init__(self, maze_loader=None, maze_drawer=None):
+        self.maze_drawer = maze_drawer
         if maze_loader:
             self.matriz = maze_loader.maze # Matriz
             self.R, self.C = np.shape(self.matriz) # Filas y Columnas
@@ -32,6 +35,7 @@ class MazeSolver:
             self.moves_column = np.array([0, 0, 1, -1])
             self.type_moven = ["Arriba", "Abajo", "Derecha", "Izquierda"]
             self.type_box = ["Libre", "Obstáculo", "Inicio", "Campo electromagnético", "Paquete"]
+            self.solution = []
 
     def reset(self):
         """Resetea los valores para poder ejecutar el algoritmo nuevamente"""
@@ -51,10 +55,10 @@ class MazeSolver:
 
         self.reset()
 
-        visited = set()
+        #visited = set()
         #cada nodo tiene (row, column, packages, cost, type moven , parent)
         self.rowCowPaDeque.append((self.sr, self.sc, frozenset(), 0, -1, None))
-        visited.add((self.sr, self.sc, frozenset(), 0, -1, None))
+        visited = [[self.sr, self.sc, frozenset(), 0, -1, None]]
 
 
 
@@ -65,8 +69,6 @@ class MazeSolver:
             # Extraemos las posiciones
             r, c, packages, cost, typemoven, parent = self.rowCowPaDeque.popleft()
             self.expanded_nodes += 1  # Se expandió un nodo
-
-            
 
             #print("En ciclo" + type(self.matriz))
 
@@ -80,6 +82,14 @@ class MazeSolver:
                 print(f"Número de objetivos alcanzados {len(packages)}")
                 print(f"Paquete encontrado en: ({r}, {c})")
 
+                # self.visited = TODO: HACER QUE LA POSICIÓN PASADA SEA UNA OPCIÓN PARA DEVOLVERSE
+                new_state = (r,c, new_packages, cost, typemoven, parent)
+                #if new_state not in visited:
+                self.rowCowPaDeque.append(new_state)
+                #visited.add(new_state)
+                visited.insert(0,new_state)
+                self.nodes_next_in_layer += 1
+
                 #self.matriz[r, c] = 0
                 #print(self.matriz)
 
@@ -87,17 +97,12 @@ class MazeSolver:
                     self.reached_end = True
                     #print(f"S quedó en: ({r}, {c})")
                     print(f"SOLUCION: Nodo=({r},{c}) - paquetes={len(new_packages)} - costo={cost} - movimiento={self.type_moven[typemoven]} - padre={parent}")
+                    self.get_solution(r, c, len(new_packages), visited)
                     break
                 #print(f"En paquete 2 {type(self.matriz)}")
 
-                # self.visited = TODO: HACER QUE LA POSICIÓN PASADA SEA UNA OPCIÓN PARA DEVOLVERSE
-                new_state = (r,c, new_packages, cost, typemoven, parent)
-                #if new_state not in visited:
-                self.rowCowPaDeque.append(new_state)
-                visited.add(new_state)
-                self.nodes_next_in_layer += 1
 
-            print(f"Nodo=({r},{c}) - paquetes={len(packages)} - costo={cost} - movimiento={self.type_moven[typemoven]} - padre={parent}")
+            #print(f"Nodo=({r},{c}) - paquetes={len(packages)} - costo={cost} - movimiento={self.type_moven[typemoven]} - padre={parent}")
             self.explore_neighbours(r, c, packages, cost, visited)
             self.nodes_left_in_layer-=1 # Quita un nodo restante
             print(f"nodos  {self.nodes_left_in_layer}")
@@ -112,6 +117,7 @@ class MazeSolver:
         if self.reached_end:
             print(f"Terminado, profundidad: {self.move_count}")
             print(f"Nodos expandidos: {self.expanded_nodes}")
+            self.run_solution()
             return self.move_count
 
         print("No se encontró la solución")
@@ -136,8 +142,51 @@ class MazeSolver:
             if any(nodo[:3] == new_state for nodo in visited): continue
             if self.matriz[rr, cc] == 1: continue
 
-            print(f"Vecino explorado: ({rr}, {cc}) ; Casilla: "+self.type_box[self.matriz[rr, cc]])
+            #print(f"Vecino explorado: ({rr}, {cc}) ; Casilla: "+self.type_box[self.matriz[rr, cc]])
             new_state = new_state + (cost + 1, i, (int(r),int(c)))
             self.rowCowPaDeque.append(new_state)
-            visited.add(new_state)
+            #visited.add(new_state)
+            visited.insert(0,new_state)
             self.nodes_next_in_layer+=1
+
+    def get_solution(self, rr, cc, lenpackage, visited):
+        print(f"get_solucition: {len(visited)}")
+        for nodo in visited:
+            r, c, packages, cost, typemoven, parent = nodo
+            #if r == rr and c == cc and lenpackage == len(packages):
+            if r == rr and c == cc:
+                self.solution.insert(0, [rr, cc])
+                if parent is not None:
+                    rr = parent[0]
+                    cc = parent[1]
+                    #lenpackage -= 1    
+                else:
+                    print("Llegamos al nodo inicial")
+                    break
+        
+        
+    
+    def run_solution(self):
+        print(f"run_solution: {len(self.solution)}")
+        previous_r = 2
+        previous_c = 1
+        
+        screen = pygame.display.get_surface()  # Obtener la superficie de la pantalla
+        
+        for nodo in self.solution:
+            r, c = nodo
+            self.maze_drawer.move_charanter(previous_r, previous_c, r, c)
+            previous_c = c
+            previous_r = r
+            
+            # Redibujar todo el laberinto
+            screen.fill((47, 40, 58))  # Color de fondo
+            self.maze_drawer.draw_maze(screen)
+            pygame.display.flip()
+            pygame.time.wait(500)
+            
+            # Procesar eventos de pygame para evitar que la ventana se congele
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
