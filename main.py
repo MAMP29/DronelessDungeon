38b75d42c303f3d5bf65d94a6357b71d5369f2a7
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-04-18 21:40:12 -0500

    Tamaño de la ventana aumentado

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from logic.MazeDrawer import MazeDrawer
 pygame.init()
 
 TILE_SIZE = 16
-WIDTH, HEIGHT = 1400, 800
+WIDTH, HEIGHT = 1450, 850
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RatCheese")
 report = "Aquí veras el reporte de los resultados"
@@ -74,6 +74,7 @@ while running:
 
         if event.type == pygame_gui.UI_FILE_DIALOG_PATH_PICKED:
             print(f"File path picked: {event.text}")
+            text_effect.set_text("Aquí veras el reporte de los resultados")
             maze_loader.file_path = event.text
             maze_loader.load_maze()
             maze_solver = MazeSolver(maze_loader, maze_drawer)
